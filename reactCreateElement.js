cb368b00126eebe00c7b3d34933290216955b9be
Colson <colsoncalifornication@gmail.com>
2024-01-18 15:53:39 +0545

    implement component with props and nested elements in the DOM

--- reactCreateElement.js
+++ reactCreateElement.js
@@ -1,5 +1,27 @@
+const Person = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('p', {}, props.occupation),
+  ]);
+};
+
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', { class: 'title' }, 'React IS rendered'),
+    React.createElement(
+      Person,
+      { name: 'Colson', occupation: 'Software Engineer' },
+      null,
+    ),
+    React.createElement(
+      Person,
+      { name: 'Shishir', occupation: 'Software Engineer' },
+      null,
+    ),
+    React.createElement(
+      Person,
+      { name: 'Samriddha', occupation: 'Trader' },
+      null,
+    ),
   ]);
 };
