602f3e6f59586b8be70cb45976330a202de1a0e3
Colson <colsoncalifornication@gmail.com>
2024-01-18 16:08:42 +0545

    refactor ReactDOM render to v18

--- renderReactDOM.js
+++ renderReactDOM.js
@@ -1,1 +1,8 @@
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+// ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
+/* @ React v18 */
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+// v18: No more access to JSX in render()
+root.render(React.createElement(App));
